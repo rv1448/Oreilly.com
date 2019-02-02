9994cbcc848f13d2d5dd17a492ea22eb9cda7bf0
rahulreddy1558 <rahulreddy1558@gmail.com>
2019-02-01 16:22:47 -0800

    intial commit

---
+++ Scala and Spark for Big Data Analytics/build.sbt
@@ -0,0 +1,5 @@
+name := "Scala-primer"
+
+version := "0.1"
+
+scalaVersion := "2.12.7"
---
+++ Scala and Spark for Big Data Analytics/src/main/scala/com/scala/test/Helloworld.scala
@@ -0,0 +1,9 @@
+package com.scala.test
+
+object Helloworld {
+  def main(args: Array[String]): Unit = {
+    println("Hello world")
+
+  }
+
+}
