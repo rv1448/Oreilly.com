82b08564b09f8034ac82726d946a805c86c8f5dc
rahulreddy1558 <rahulreddy1558@gmail.com>
2019-02-08 19:45:05 -0800

    Spark scala

--- Spark-Scala/src/main/scala/com/spark/scala/Helloworld.scala
+++ Spark-Scala/src/main/scala/com/spark/scala/Helloworld.scala
@@ -1,8 +1,12 @@
 package com.spark.scala
 
 object Helloworld {
+  def noargs() = "no args"
+// liternal value of unit is ()
+  //
  def main(args: Array[String]): Unit = {
   println("Hello World")
+   noargs
  }
 
 }
