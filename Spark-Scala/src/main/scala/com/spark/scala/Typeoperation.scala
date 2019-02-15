c530eaf90b1df8de2da022cc8b84db165228ed95
rahulreddy1558 <rahulreddy1558@gmail.com>
2019-02-15 16:03:13 -0500

    Spark scala

---
+++ Spark-Scala/src/main/scala/com/spark/scala/Typeoperation.scala
@@ -0,0 +1,20 @@
+package com.spark.scala
+
+/**
+  * asInstanceof
+  * isInstanceof
+  ** to<type>
+  *   getclass
+  */
+
+object Typeoperation {
+
+  def main(args: Array[String]): Unit = {
+    123.asInstanceOf[Long]
+    println(123.toLong)
+    123.toChar
+    123.toString()
+    213.toDouble
+  }
+
+}
