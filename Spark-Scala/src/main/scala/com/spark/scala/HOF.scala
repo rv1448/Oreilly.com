35b1b8d347f392379b7be29287b5e8b42c101042
rahulreddy1558 <rahulreddy1558@gmail.com>
2019-02-25 21:32:17 -0800

    Spark scala

---
+++ Spark-Scala/src/main/scala/com/spark/scala/HOF.scala
@@ -0,0 +1,52 @@
+package com.spark.scala
+
+object HOF {
+
+
+
+  def fibanacci(x:Int):Int = {
+//    x match {
+//      case
+//    }
+    ???
+  }
+
+
+  def factorial(x:Int):Int = {
+    if (x ==0 ) 1 else x * factorial(x-1)
+  }
+
+  def findtheindex[a](l:Array[a],p:a => Boolean):Int = {
+//    if(m:Int >= l.length)
+    def loop(n:Int):Int={
+      if (n >= l.length) -1
+      else if (p(l(n))) n
+      else loop(n+1)
+    }
+
+    loop(0)
+  }
+
+  def comparision(a:Array[Int],p:(Int,Int) => Boolean ) = {
+    def loop(n:Int):Boolean = {
+      if(n == a.length-1) true
+      else if (p(a(n),a(n+1))) {println(n);loop(n+1)}
+      else false
+
+    }
+
+    loop(0)
+  }
+
+
+  def main(args: Array[String]): Unit = {
+//    println( factorial(5))
+
+//    println(findtheindex(Array(1,2,3,4,5,6,75,5,7,9),(x:Int) => if (x ==9) true else false))
+    println(comparision(Array(1,2,3,4,5,10,9),(x:Int, y:Int) => if (x < y) true else false))
+
+    println(Array(1,2,3,4,5,6,9).length)
+
+  }
+
+}
---
+++ Spark-Scala/src/main/scala/com/spark/scala/Stream.java
@@ -0,0 +1,23 @@
+package com.spark.scala;
+import java.net.*;
+import java.io.*;
+
+
+public class Stream {
+
+    public static void main(String[] args)  throws IOException{
+
+
+        URL url = new URL("https://stream.meetup.com/2/rsvps");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(
+                conn.getInputStream()));
+
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            System.out.println(inputLine);
+
+
+
+    }
+}
