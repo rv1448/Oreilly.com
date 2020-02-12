20993895af9f26ea5e789e13a735fbe3dc813c4e
rahulreddy1558 <rahulreddy1558@gmail.com>
2020-02-11 22:57:53 -0800

    Generics

--- ProgrammingLambdasandStreams/src/README.md
+++ ProgrammingLambdasandStreams/src/README.md
@@ -1,1 +1,9 @@
 ## GENERICS
+
+* Wildcard with Extends
+    ```java
+    interface Collection<E>{
+  public boolean addAll(Collection< ? extends E> c);
+  }
+  /* " ? extends E" means that it ok to add 
+  ```
--- ProgrammingLambdasandStreams/src/main/java/com/java/generics/ListNum.java
+++ ProgrammingLambdasandStreams/src/main/java/com/java/generics/ListNum.java
@@ -1,8 +1,26 @@
 package com.java.generics;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 public class ListNum {
 
+    public static  <E>  void  addAll(Collection<? extends E> collection){
+        for (E e: collection){
+
+        }
+
+    }
+
+
+
     public static void main(String[] args) {
+        List<Number> nums = new ArrayList<Number>();
+        List<Integer> ints = Arrays.asList(1, 2);
+        List<Double> dbls = Arrays.asList(2.78, 3.14);
+
 
     }
 }
