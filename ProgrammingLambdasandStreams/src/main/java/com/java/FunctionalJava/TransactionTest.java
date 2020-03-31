b4535bf2cb014c60b5651d30abb08fe78587ab2a
rahulreddy1558 <rahulreddy1558@gmail.com>
2020-03-31 01:39:02 -0400

    Modern Java

---
+++ ProgrammingLambdasandStreams/src/main/java/com/java/FunctionalJava/TransactionTest.java
@@ -0,0 +1,46 @@
+package com.java.FunctionalJava;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class TransactionTest {
+    public static void main(String[] args) {
+        Trader raoul = new Trader("Raoul", "Cambridge");
+        Trader mario = new Trader("Mario","Milan");
+        Trader alan = new Trader("Alan","Cambridge");
+        Trader brian = new Trader("Brian","Cambridge");
+        List<Transaction> transactions = Arrays.asList(
+                new Transaction(brian, 2011, 300),
+                new Transaction(raoul, 2012, 1000),
+                new Transaction(raoul, 2011, 400),
+                new Transaction(mario, 2012, 710),
+                new Transaction(mario, 2012, 700),
+                new Transaction(alan, 2012, 950)
+        );
+
+        transactions.stream()
+                .filter(t -> t.getYear() ==2011)
+                .sorted(Comparator.comparing(Transaction::getValue))
+                .collect(Collectors.toList());
+
+        transactions.stream()
+                .map(t ->t.getTrader().getCity())
+                .distinct()
+                .collect(Collectors.toList());
+
+        transactions.stream()
+                .map(t -> t.getTrader())
+                .takeWhile(t -> t.getCity().equals("Cambridge"))
+                .sorted(Comparator.comparing(Trader::getName))
+                .forEach(t -> System.out.println(t));
+
+        Optional<Integer> k = transactions.stream()
+                .map(t-> t.getValue())
+                .max( (t,u) -> t.compareTo(u) );
+        System.out.println(k.get());
+//
+    }
+}
