9c493b2dacae3007246f4941d9872e3ea60f76dd
rahulreddy1558 <rahulreddy1558@gmail.com>
2020-06-14 01:44:33 -0400

    initial commit

---
+++ ScalaSandbox/src/main/scala/FileTrait.scala
@@ -0,0 +1,20 @@
+object FileTrait extends App{
+  import java.io.File
+  import scala.io.Source
+  trait  Fileops{
+    self: File =>
+  def istextfile:Boolean = this.getName.endsWith(".txt")
+    def readtext:Iterator[String] = {
+      Source.fromFile(this).getLines();
+    }
+    def printlines:Unit = {
+      this.readtext foreach println
+    }
+  }
+  import java.io.File;
+  val file = new File("/Users/RahulReddy/Desktop/Oreilly_Training/Oreilly.com/ScalaSandbox/area.txt") with Fileops;
+
+
+  file.printlines
+
+}
