e962da8e8b4a3803fbd9548100d71724174550e6
rahulreddy1558 <rahulreddy1558@gmail.com>
2020-06-14 21:13:29 -0400

    initial commit

--- ScalaSandbox/src/main/scala/Interest.scala
+++ ScalaSandbox/src/main/scala/Interest.scala
@@ -8,4 +8,43 @@ object Interest extends App{
   val increment = (x:Int) => x + 1;
   println(increment(10))
 
+  val simpleinterest = (p:Double,i:Double,r:Double) => {p * i * r}:Double
+  val compountinterest = (p:Double,i:Double,r:Double) => (p * (Math.pow((1+r),r))) - p
+
+  def interestReport(p:Double,i:Double,r:Double,calc:(Double,Double,Double) => Double):Double = {
+    calc(p,i,r)
+  }
+
+
+  def convertStringtoInt(x:String):Option[Int] = {
+    try {
+      Some(x.toInt)
+    }
+    catch   {
+      case num: NumberFormatException => None
+    }
+  }
+
+
+  def convertStringToIntEither(x:String):Either[String,Int] = {
+    try{
+      Right(x.toInt)
+    }
+    catch {
+      case e : Throwable => Left(s"${e.getMessage}")
+    }
+  }
+
+
+
+
+  val print = convertStringtoInt("ab").getOrElse(0)
+
+  convertStringtoInt("145") match {
+    case Some(x) => println(s"SUccessfully casted into Integer${x}")
+    case _ => println("exception")
+  }
+
+  convertStringtoInt("abc").map(x => x*2).map(println)
+  convertStringToIntEither("01").map(number => println(number))
 }
